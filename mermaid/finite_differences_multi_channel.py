983072e56f5ab33727c7ac6704ab361bf2bbbd25
Alexander <alex58733@live.dk>
2021-03-12 10:58:13 +0100

    minor deletions

--- mermaid/finite_differences_multi_channel.py
+++ mermaid/finite_differences_multi_channel.py
@@ -550,7 +550,4 @@ class FD_torch_multi_channel(FD_multi_channel):
         :param A: input array
         :return: shape/size
         """
-        if type(A.size) == int:
-            print("John")
-            pass
         return A.size()
